d9d89e40a0c70eb0ec226064adfc3f9cfd651852
maytopacka <rpascua.synsoftech@gmail.com>
2017-05-30 20:00:36 +0800

    first commit

---
+++ app/Http/Controllers/ServicesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ServicesController extends Controller
+{
+     public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('services');
+    }
+}
